179b7ff0fa9a294238ff1aa7bfc12e3a36678bf3
Aravinth Ranganathan <32588284+AravinthRanganathan@users.noreply.github.com>
2018-07-22 09:42:05 +0530

    Update _02_CgpaCalculator.java

--- Project/CGPA Calculator/_02_CgpaCalculator.java
+++ Project/CGPA Calculator/_02_CgpaCalculator.java
@@ -20,7 +20,7 @@ public class _02_CgpaCalculator {
 			int number_of_subject=sc.nextInt();
 			float tot=0f,totsgpa=0.0f;
 			Map<String,Integer> map=new LinkedHashMap<String,Integer>();
-			System.out.println("Enter the Subject Names with grade :\n(Note:The grades are entered as below format) \nGrades->S,A,B,C,D,E,AB,RA-F\nS->Grade 10\nA->Grade 9\nB->Grade 8\nC->Grade 7\nD->Grade 6\nE->Grade 5\nAB->Absent\nRA-F->Reappear");
+			System.out.println("Enter the Subject Names with grade :\n(Note:The grades are entered as below format) \nGrades->S,A,B,C,D,E,AB,RA-F\nS->Credit points-10\nA->Credit points-9\nB->Credit points-8\nC->Credit points-7\nD->Credit points-6\nE->Credit points-5\nAB->Absent\nRA-F->Reappear");
 			String str[]=new String[number_of_subject];
 			int key[]=new int[number_of_subject];
 			for(int i=0;i<number_of_subject;i++){
